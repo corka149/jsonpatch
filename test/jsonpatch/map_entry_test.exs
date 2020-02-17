4d8048a60ad3d12aeead768354366e3cf1653605
corka149 <corka149@mailbox.org>
2020-02-17 19:34:39 +0100

    Added test for map entries with map array

--- test/jsonpatch/map_entry_test.exs
+++ test/jsonpatch/map_entry_test.exs
@@ -8,9 +8,9 @@ defmodule Jsonpatch.MapEntryTest do
     map_entries = MapEntry.to_map_entries(source)
 
     assert [
-      %MapEntry{path: "/name", value: "Bob"},
-      %MapEntry{path: "/age", value: 27}
-    ] = map_entries
+             %MapEntry{path: "/name", value: "Bob"},
+             %MapEntry{path: "/age", value: 27}
+           ] = map_entries
   end
 
   test "convert a two dimensional map to map entries" do
@@ -19,11 +19,11 @@ defmodule Jsonpatch.MapEntryTest do
     map_entries = MapEntry.to_map_entries(source)
 
     assert [
-      %MapEntry{path: "/name", value: "Bob"},
-      %MapEntry{path: "/age", value: 27},
-      %MapEntry{path: "/address/street", value: "Somestreet"},
-      %MapEntry{path: "/address/city", value: "Somewhere"}
-    ] = map_entries
+             %MapEntry{path: "/name", value: "Bob"},
+             %MapEntry{path: "/age", value: 27},
+             %MapEntry{path: "/address/street", value: "Somestreet"},
+             %MapEntry{path: "/address/city", value: "Somewhere"}
+           ] = map_entries
   end
 
   test "convert a four dimensional map to map entries" do
@@ -31,8 +31,8 @@ defmodule Jsonpatch.MapEntryTest do
     map_entries = MapEntry.to_map_entries(source)
 
     assert [
-      %MapEntry{path: "/a/b/c/d", value: "e"}
-    ] = map_entries
+             %MapEntry{path: "/a/b/c/d", value: "e"}
+           ] = map_entries
   end
 
   test "convert a two dimensional map with a value array" do
@@ -41,15 +41,40 @@ defmodule Jsonpatch.MapEntryTest do
     map_entries = MapEntry.to_map_entries(source)
 
     assert [
-      %MapEntry{path: "/name", value: "Bob"},
-      %MapEntry{path: "/languages/1", value: "Erlang"},
-      %MapEntry{path: "/languages/0", value: "Elixir"},
-      %MapEntry{path: "/age", value: 27},
-    ] = map_entries
+             %MapEntry{path: "/name", value: "Bob"},
+             %MapEntry{path: "/languages/1", value: "Erlang"},
+             %MapEntry{path: "/languages/0", value: "Elixir"},
+             %MapEntry{path: "/age", value: 27}
+           ] = map_entries
   end
 
   test "convert a two dimensional map with a map array" do
+    employers = [
+      %{"name" => "Somecompany1", "city" => "Somewhere"},
+      %{"name" => "Othercompany", "city" => "Otherwhere"}
+    ]
 
+    source = %{"name" => "Alice", "employers" => employers, "age" => 26}
+    map_entries = MapEntry.to_map_entries(source)
+
+    target = [
+      %Jsonpatch.MapEntry{path: "/name", value: "Alice"},
+      %Jsonpatch.MapEntry{
+        path: "/employers/1/name",
+        value: "Othercompany"
+      },
+      %Jsonpatch.MapEntry{
+        path: "/employers/1/city",
+        value: "Otherwhere"
+      },
+      %Jsonpatch.MapEntry{
+        path: "/employers/0/name",
+        value: "Somecompany1"
+      },
+      %Jsonpatch.MapEntry{path: "/employers/0/city", value: "Somewhere"},
+      %Jsonpatch.MapEntry{path: "/age", value: 26}
+    ]
+
+    assert target = map_entries
   end
-
 end
