e4c219f47b8a9cb2eba5eeb8166c0765072f6a7e
corka149 <corka149@mailbox.org>
2020-05-10 12:03:06 +0200

    Added Operation behaviour

---
+++ lib/jsonpatch/operation/operation.ex
@@ -0,0 +1,16 @@
+defmodule Jsonpatch.Operation do
+  @moduledoc """
+  Defines behaviour for apply a patch to a struct.
+  """
+
+  alias Jsonpatch.Operation.Add
+  alias Jsonpatch.Operation.Remove
+  alias Jsonpatch.Operation.Replace
+
+  @typedoc """
+  A valid Jsonpatch operation by RFC 6902
+  """
+  @type t :: Add.t() | Remove.t() | Replace.t()
+
+  @callback apply(Jsonpatch.operation()) :: map()
+end
