ece7714a3315eb1dea7cc9a7f0871eaf84afbd57
corka149 <corka149@mailbox.org>
2020-05-13 22:42:56 +0200

    Implemented apply remove operation

--- lib/jsonpatch/operation/remove.ex
+++ lib/jsonpatch/operation/remove.ex
@@ -1,4 +1,34 @@
 defmodule Jsonpatch.Operation.Remove do
+  @behaviour Jsonpatch.Operation
+
   @enforce_keys [:path]
   defstruct [:path]
+
+  @doc """
+  Removes the element referenced by the JSON patch path.
+
+  ## Examples
+
+      iex> remove = %Jsonpatch.Operation.Remove{path: "/a/b"}
+      iex> target = %{"a" => %{"b" => %{"c" => "Bob"}}, "d" => false}
+      iex> Jsonpatch.Operation.Remove.apply_op(remove, target)
+      %{"a" => %{}, "d" => false}
+  """
+  @impl true
+  def apply_op(%Jsonpatch.Operation.Remove{path: path}, target) do
+    # The first element is always "" which is useless.
+    [_ | fragments] = String.split(path, "/")
+    do_remove(target, fragments)
+  end
+
+  # ===== ===== PRIVATE ===== =====
+
+  defp do_remove(target, [fragment | []]) do
+    {_, purged_map} = Map.pop(target, fragment)
+    purged_map
+  end
+
+  defp do_remove(target, [fragment | tail]) do
+    Map.update!(target, fragment, &do_remove(&1, tail))
+  end
 end
---
+++ test/jsonpatch/operation/remove_test.exs
@@ -0,0 +1,4 @@
+defmodule Jsonpatch.Operation.RemoveTest do
+  use ExUnit.Case
+  doctest Jsonpatch.Operation.Remove
+end
