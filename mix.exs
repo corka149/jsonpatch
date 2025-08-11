ac77903eaf4b7a699a1b8ce0ca70ab5f074cb00a
corka149 <corka149@mailbox.org>
2025-08-11 13:41:15 +0200

    Prepare 2.3.0 release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Jsonpatch.MixProject do
       app: :jsonpatch,
       name: "Jsonpatch",
       description: "Implementation of RFC 6902 in pure Elixir",
-      version: "2.2.2",
+      version: "2.3.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
